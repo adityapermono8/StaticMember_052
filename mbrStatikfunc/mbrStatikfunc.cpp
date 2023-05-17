aff2537db597217ad65e1c274b82c77903ee0e6a
adityapermono8 <adityapermono8@gmail.com>
2023-05-17 19:08:27 +0700

    mencetak mhs1,mhs2,mhs3,mhs4 dan mengakses nim dari luar objek menggunakan metode statik

--- mbrStatikfunc/mbrStatikfunc.cpp
+++ mbrStatikfunc/mbrStatikfunc.cpp
@@ -36,3 +36,13 @@ int main() {
 	mahasiswa::setNim(9); //mengakses nim melalui static memberfunction "setNim"
 	mahasiswa mhs3("Andi Janu");
 	mahasiswa mhs4("Joko Wahono");
+
+	mhs1.printAll();
+	mhs2.printAll();
+	mhs3.printAll();
+	mhs4.printAll();
+
+	cout << "akses dari luar object = " << mahasiswa::getNim() << endl; //mengakses nim melalui static member function "getNim"
+
+	return 0;
+}
